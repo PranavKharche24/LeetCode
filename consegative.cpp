f9803974b3ffb0b4a0e9bd489f734a643c0558c8
PranavKharche24 <2022bcs020@sggs.ac.in>
2024-10-09 12:02:48 +0530

    ConsegativeProblem

---
+++ consegative.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        // Implementation will go here
+        return 0; // Placeholder return
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {100, 4, 200, 1, 3, 2};
+    int result = solution.longestConsecutive(nums);
+    
+    cout << "The longest consecutive sequence is: " << result << endl;
+    
+    return 0;
+}
